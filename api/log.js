fdffc04becd6f28f83c30e4db352e23bce39dc93
Random <exploitrose23@gmail.com>
2025-09-25 13:05:20 +0800

    Create log.js

---
+++ api/log.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log('Logged visitor location:', req.body);
+    res.status(200).json({ status: 'success' });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
